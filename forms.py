636d140ee947de3757ac7232c36d173a937c6e6c
lilukai1 <50470636+lilukai1@users.noreply.github.com>
2020-12-16 13:57:04 -0600

    Label fix

--- forms.py
+++ forms.py
@@ -7,26 +7,24 @@ from wtforms.validators import (DataRequired, Regexp, ValidationError,
 from models import Entry
 
 
-
 class EntryForm(FlaskForm):
     title = StringField(label="Title", validators=[
                                 DataRequired(), 
                                 Length(max=50)
                             ])
     content = TextAreaField(label="Project Description",
-                            
                             validators=[
                                 DataRequired()
                             ])
-    timestamp = DateField(label="Project Date:",
+    timestamp = DateField(label="Project Date",
                         default=date.today(),
                         validators=[
                                 DataRequired(),
                             ])
 
-    time_spent = IntegerField(label="Time spent in minutes.",
-                                validators=[
-                                DataRequired(),
+    time_spent = IntegerField(label="Time spent in minutes",
+                            validators=[
+                            DataRequired(),
                             ])
     resources = TextField(label="Resources",
                         validators=[
